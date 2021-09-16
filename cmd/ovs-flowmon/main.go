5e6aa75165cd37feeebdf58ddd0d53b33fe00065
Adrian Moreno <amorenoz@redhat.com>
2021-09-16 18:16:38 +0200

    Add support for modifying aggregates
    The flowtable would now have a list of flows and a list of aggregates.
    Aggregates can be updated dynamically (without modifying the flows) and
    they are recomputed
    
    Signed-off-by: Adrian Moreno <amorenoz@redhat.com>

--- cmd/ovs-flowmon/main.go
+++ cmd/ovs-flowmon/main.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"net/url"
 	"strconv"
+	"sync"
 
 	"github.com/gdamore/tcell/v2"
 	"github.com/netsampler/goflow2/format"
@@ -23,29 +24,54 @@ var (
 	app       *tview.Application
 	started   bool = false
 
-	default_fields []string = []string{"InIf", "OutIf", "SrcMac", "DstMac", "VlanID", "Etype", "SrcAddr", "DstAddr", "Proto", "SrcPort", "DstPort", "FlowDirection"}
+	fieldList  []string = []string{"InIf", "OutIf", "SrcMac", "DstMac", "VlanID", "Etype", "SrcAddr", "DstAddr", "Proto", "SrcPort", "DstPort", "FlowDirection"}
+	aggregates map[string]bool
 )
 
 type FlowTable struct {
+	// mutex to protect aggregates (not flows)
+	mutex      sync.RWMutex
 	flows      []*flowmon.FlowInfo
 	aggregates []*flowmon.FlowAggregate
-	keys       []string
+	// Keeping both the list and the map for efficiency
+	aggregateKeyList []string
+	aggregateKeyMap  map[string]bool
+	keys             []string
 }
 
 func NewFlowTable() *FlowTable {
+	aggregateKeyList := []string{}
+	for _, field := range fieldList {
+		if aggregates[field] {
+			aggregateKeyList = append(aggregateKeyList, field)
+		}
+	}
 	return &FlowTable{
-		flows:      make([]*flowmon.FlowInfo, 0),
-		aggregates: make([]*flowmon.FlowAggregate, 0),
-		keys:       default_fields,
+		mutex:            sync.RWMutex{},
+		flows:            make([]*flowmon.FlowInfo, 0),
+		aggregates:       make([]*flowmon.FlowAggregate, 0),
+		aggregateKeyList: aggregateKeyList,
+		aggregateKeyMap:  aggregates,
+		keys:             fieldList,
 	}
 }
 
-func (ft *FlowTable) InsertFlow(f *flowmon.FlowInfo) {
-	//ft.flows = append(ft.flows, f)
-	// TODO: Different kind of sorting
-	// For now, prepend the last received flow at the top
-	log.Debugf("Inserting %#v", f)
-	ft.flows = append(ft.flows, f)
+func (ft *FlowTable) UpdateKeys(aggregates map[string]bool) {
+	aggregateKeyList := []string{}
+	for _, field := range fieldList {
+		if aggregates[field] {
+			aggregateKeyList = append(aggregateKeyList, field)
+		}
+	}
+	ft.mutex.Lock()
+	defer ft.mutex.Unlock()
+	ft.aggregateKeyList = aggregateKeyList
+	ft.aggregateKeyMap = aggregates
+	// Need to recompute all aggregations
+	ft.aggregates = make([]*flowmon.FlowAggregate, 0)
+	for _, flow := range ft.flows {
+		ft.ProcessFlow(flow)
+	}
 }
 
 func (ft *FlowTable) Draw(tv *tview.Table) {
@@ -77,12 +103,20 @@ func (ft *FlowTable) Draw(tv *tview.Table) {
 	tv.SetCell(0, col, cell)
 	col += 1
 
+	ft.mutex.RLock()
+	defer ft.mutex.RUnlock()
 	for i, agg := range ft.aggregates {
-		for col, key := range ft.keys {
-			fieldStr, err := agg.GetFieldString(key)
-			if err != nil {
-				log.Error(err)
-				fieldStr = "err"
+		for col, key := range fieldList {
+			var fieldStr string
+			var err error
+			if !ft.aggregateKeyMap[key] {
+				fieldStr = "-"
+			} else {
+				fieldStr, err = agg.GetFieldString(key)
+				if err != nil {
+					log.Error(err)
+					fieldStr = "err"
+				}
 			}
 			cell = tview.NewTableCell(fieldStr).SetTextColor(tcell.ColorWhite).SetAlign(tview.AlignLeft).SetSelectable(true)
 			tv.SetCell(1+i, col, cell)
@@ -118,12 +152,19 @@ func (ft *FlowTable) Draw(tv *tview.Table) {
 	}
 }
 
-func (ft *FlowTable) ProcessFlow(msg *flowmessage.FlowMessage) {
+func (ft *FlowTable) ProcessMessage(msg *flowmessage.FlowMessage) {
 	log.Debugf("Processing Flow Message: %+v", msg)
 
 	flowInfo := flowmon.NewFlowInfo(msg)
 	ft.flows = append(ft.flows, flowInfo)
 
+	ft.mutex.Lock()
+	defer ft.mutex.Unlock()
+	ft.ProcessFlow(flowInfo)
+}
+
+// Caller must hold mutex
+func (ft *FlowTable) ProcessFlow(flowInfo *flowmon.FlowInfo) {
 	var matched bool = false
 	var err error = nil
 	for _, agg := range ft.aggregates {
@@ -138,7 +179,7 @@ func (ft *FlowTable) ProcessFlow(msg *flowmessage.FlowMessage) {
 	}
 	if !matched {
 		// Create new Aggregate for this flow
-		newAgg := flowmon.NewFlowAggregate(ft.keys)
+		newAgg := flowmon.NewFlowAggregate(ft.aggregateKeyList)
 		if match, err := newAgg.AppendIfMatches(flowInfo); !match || err != nil {
 			log.Fatal(err)
 		}
@@ -233,7 +274,7 @@ func (d *Dispatcher) Send(key, data []byte) error {
 		log.Errorf("Wrong Flow Message (%s) : %s", err.Error(), string(data))
 		return err
 	}
-	d.flowTable.ProcessFlow(&msg)
+	d.flowTable.ProcessMessage(&msg)
 	app.QueueUpdateDraw(func() {
 		d.flowTable.Draw(tableView)
 	})
@@ -244,8 +285,16 @@ type Listener interface {
 	OnNewFlow(flow *flowmessage.FlowMessage)
 }
 
+func UpdateFlowTableKeys() {
+
+}
 func main() {
+	aggregates = make(map[string]bool, 0)
+	for _, key := range fieldList {
+		aggregates[key] = true
+	}
 	app = tview.NewApplication()
+	//pages := tview.NewPages()
 	app.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
 		if event.Key() == tcell.KeyCtrlC {
 			app.Stop()
@@ -260,6 +309,11 @@ func main() {
 	log.SetOutput(status)
 
 	// Top Menu
+	menu := tview.NewFlex()
+	menuList := tview.NewList().
+		ShowSecondaryText(false)
+	menu.AddItem(menuList, 0, 2, true)
+
 	start := func() {
 		log.Info("Started flow processing")
 		if !started {
@@ -275,10 +329,32 @@ func main() {
 	logs := func() {
 		app.SetFocus(status)
 	}
-	menuList := tview.NewList().ShowSecondaryText(false).
-		AddItem("Start", "", 's', start).
+
+	show_aggregate := func() {
+		// Make columns selectable
+		tableView.SetSelectable(false, true)
+		app.SetFocus(tableView)
+		tableView.SetSelectedFunc(func(row, col int) {
+			colName := tableView.GetCell(0, col).Text
+			// Toggle aggregate and update flow table
+			aggregates[colName] = !aggregates[colName]
+			flowTable.UpdateKeys(aggregates)
+			tableView.Clear()
+			flowTable.Draw(tableView)
+
+			// Restore selectable columns and SelectedFunc
+			tableView.SetSelectable(true, false)
+			tableView.SetSelectedFunc(func(row, col int) {
+				app.SetFocus(menuList)
+			})
+			app.SetFocus(menuList)
+		})
+	}
+
+	menuList.AddItem("Start", "", 's', start).
 		AddItem("Stop", "", 't', stop).
-		AddItem("Logs", "", 'l', logs)
+		AddItem("Logs", "", 'l', logs).
+		AddItem("Add/Remove Fields from aggregate", "", 'a', show_aggregate)
 	menuList.SetBorderPadding(1, 1, 2, 2)
 	tableView.SetDoneFunc(func(key tcell.Key) {
 		app.SetFocus(menuList)
@@ -289,11 +365,11 @@ func main() {
 	status.SetDoneFunc(func(key tcell.Key) {
 		app.SetFocus(menuList)
 	})
-	menuList.SetBorder(true).SetBorderPadding(1, 1, 2, 0).SetTitle("Menu")
+	menu.SetBorder(true).SetBorderPadding(1, 1, 2, 0).SetTitle("Menu")
 	tableView.SetBorder(true).SetBorderPadding(1, 1, 2, 0).SetTitle("Flows")
 	status.SetBorder(true).SetBorderPadding(1, 1, 2, 0).SetTitle("Logs")
 
-	flex := tview.NewFlex().SetDirection(tview.FlexRow).AddItem(menuList, 0, 2, true).AddItem(tableView, 0, 5, false).AddItem(status, 0, 1, false)
+	flex := tview.NewFlex().SetDirection(tview.FlexRow).AddItem(menu, 0, 2, true).AddItem(tableView, 0, 5, false).AddItem(status, 0, 1, false)
 
 	app.SetRoot(flex, true).SetFocus(flex)
 
